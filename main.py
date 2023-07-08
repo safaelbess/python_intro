ee413e851a10540c48cafe3eba5ecba0b549dc0a
safaelbess <136888825+safaelbess@users.noreply.github.com>
2023-07-08 01:58:25 -0700

    learn about index & slice for string

---
+++ main.py
@@ -0,0 +1,36 @@
+
+# var1="value"
+
+# print(var1)
+# var2=22
+# var3=2.4
+# var4=True
+# print(var1,var2,var3,var4)
+# print(type(var1))
+# print(type(var2))
+# print(type(var3))
+# print(type(var4))
+# var5=9+9j
+# print(var5)
+# print(type(var5))
+# print(var1[0])
+
+
+
+# print(var1[1:3])
+# print(var1[1:])
+# print(len(var1))
+num1=6
+print(num1)
+num1=10
+print(num1)
+str1="hello"
+print(str1)
+
+st1="hello"
+print(st1)
+st1='y'+st1[1:len(st1)]
+print(st1)
+str2="hellow"
+str2=str2[0:2]+'mm'+str2[4:]
+print(str2)

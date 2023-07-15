922fc086dda588ad8f29eda1eac917df146bf62c
safaelbess <136888825+safaelbess@users.noreply.github.com>
2023-07-15 01:07:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,19 @@
-
-# var1="value"
-
-# print(var1)
-# var2=22
-# var3=2.4
-# var4=True
-# print(var1,var2,var3,var4)
-# print(type(var1))
-# print(type(var2))
-# print(type(var3))
-# print(type(var4))
-# var5=9+9j
-# print(var5)
-# print(type(var5))
-# print(var1[0])
-
-
-
-# print(var1[1:3])
-# print(var1[1:])
-# print(len(var1))
-# num1=6
-# print(num1)
-# num1=10
-# print(num1)
-# str1="hello"
-# print(str1)
-
-# st1="hello"
-# print(st1)
-# st1='y'+st1[1:len(st1)]
-# print(st1)
-# str2="hellow"
-# str2=str2[0:2]+'mm'+str2[4:]
-# print(str2)
-num1=int(input('enter a number'))
-if num1>1:   
-  
-  
-  
-    for i in range(2,num1):
-
-     if num1%i==0:
-      print('num is not prime')     
-      break    
-    else:
-      print('prime')
-      
-      
+# if 1==2:
+#    print('1')
+# elif 1==3:
+#    print('2')
+# else:
+#    print('unknown')      
+   
+#    switch:
+#      case1:
+#        print('1')
+#      case2:
+#        print('2')
+#      default:
+#        print('unknown')    
+num=0
+while num < 10:
+ print(num)
+ num=num+1
+ 

ff60bcb97b00133c23105a08c8b510a3ab3b6666
safaelbess <136888825+safaelbess@users.noreply.github.com>
2023-07-19 00:28:09 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,51 @@
-# if 1==2:
-#    print('1')
-# elif 1==3:
-#    print('2')
-# else:
-#    print('unknown')      
+# # # if 1==2:
+# # #    print('1')
+# # # elif 1==3:
+# # #    print('2')
+# # # else:
+# # #    print('unknown')      
    
-#    switch:
-#      case1:
-#        print('1')
-#      case2:
-#        print('2')
-#      default:
-#        print('unknown')    
-num=0
-while num < 10:
- print(num)
- num=num+1
+# # #    switch:
+# # #      case1:
+# # #        print('1')
+# # #      case2:
+# # #        print('2')
+# # #      default:
+# # #        print('unknown')    
+# # num=0
+# # while num < 10:
+# #  print(num)
+# #  num=num+1
+# secretworld='python'
+# c=0
+# check = True
+# while check :
+     
+ 
+#    userinput = print('enter your word')
+#    c= c + 1
+# if userinput==secretworld:
+#    print('correct') 
+# else:
+#      print('wrong')
+#      if c==2:
+#          check=False
+#          print('you have reached the limit')
+#          break
+  
+ counter =0
+ while counter<5:
+     userinput=('enter your string')
+     print('-'*50)
+     for i in range(0,len):
+         ifuserinput[i]=='l'or  userinput[i]=='u'
+            userinput=userinput.replace(userinput[i],'*')
+            print(userinput[i])
+            counter=counter+1
+             
+         
+          
+    
+
+                
  
